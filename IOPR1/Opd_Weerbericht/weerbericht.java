5f561ce31fe2afd0f220bcd8b7ee192d9341602d
sm2011 <23eagle@live.com>
2013-10-21 14:38:47 +0200

    gefixt

--- IOPR1/Opd_Weerbericht/weerbericht.java
+++ IOPR1/Opd_Weerbericht/weerbericht.java
@@ -3,17 +3,26 @@ public class weerbericht {
 
 	
 	// atriubten 
+	private String berichtenarray;
 	String berichten ; 
 	public weerbericht() {
 		
-		String [] array = new String[7] ;
+		this.berichtenarray = new String[7] ;
 	
 		berichten = " zon, regen, bewolkt";
 		for ( int i= 0 ; i <array.length; i++) 
 		{
 			
+			System.out.println(" Het weer voor de volgnde week is>:");
 			
 			
 		}
+	}
+	
+	public String getDagBericht()
+	{
+		
+		
+		retrun berichten;
 	}
 }
