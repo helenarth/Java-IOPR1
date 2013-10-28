2f0468f77a1ef1fa03e89f51e42e134344888f29
sm2011 <23eagle@live.com>
2013-10-28 13:55:45 +0100

    weer opdracht

--- IOPR1/weerbericht/Appmain.java
+++ IOPR1/weerbericht/Appmain.java
@@ -1,10 +1,17 @@
 public class Appmain {
 	
 	public static void main(String[] args) {
+		//maak een nieuw weerbericht object
+		
 		weerbericht wb  =new weerbericht();
 		
 		
-		System.out.println(" Maandag: Bewokt \n" + " Dinsdag: zon \n" + " Woensdag: regen\n");
+		//print weerbericht voor maandag, dinsdag en woensdag
+		System.out.println( "Maandag" +wb.getDagBericht(1));
+		System.out.println( "Dinsdag" +wb.getDagBericht(2));
+		System.out.println( "Woensdag" +wb.getDagBericht(3));
+		
+		
 	}
 
 
--- IOPR1/weerbericht/weerbericht.java
+++ IOPR1/weerbericht/weerbericht.java
@@ -1,28 +1,50 @@
-
 public class weerbericht {
 
 
-	// atriubten 
+	//Het attribuut dat naar een array van String objecten gaat verwijzen
 	
-	String berichten;
+	private String [] berichten;
+	
 	public weerbericht() {
-		
-		String[] berichtenarray = new String[7] ;
 	
-		berichten = " Zon";
-		for ( int i= 0 ; i <berichtenarray.length; i++) 
+		
+		//maak een array van 7 String objecten, berichten verwijst nu naar deze array
+		berichten  = new String[7];
+	
+		//vul de berichtenarray willekeurig met 3 verschillende
+		//weersomstandigheden: “zon”, “regen” en “bewolkt”
+		
+		//loop door de array heen
+		for ( int i= 0 ; i <berichten.length; i++) 
 		{
-			
-	//		System.out.println(" Het weer voor de volgnde week is:" + berichten);
+			//maak een willekeurig getal van 0 tot 3 (0, 1 en 2)
+			int weer = (int) (Math.random() * 3);
+			//elk getal stelt een weertype voor, en wordt in de array opgeslagen
+			if (weer ==0 ) {
+				berichten[i] = "zon";
+			}
+			else if( weer == 1)
+			{
+				
+				berichten[i] = " regen";
+				
+				
+			}
+			else if ( weer == 2)
+			{
+				
+				berichten[i] = " Bewolkt";
+				
+			}
 			
 			
 		}
 	}
-	
-	public String getDagBericht()
+	//methode om het weertype (als String) op te halen van een bepaalde dag
+	public String getDagBericht( int dag)
 	{
 		
 		
-		return berichten;
+		return berichten[dag];
 	}
 }
