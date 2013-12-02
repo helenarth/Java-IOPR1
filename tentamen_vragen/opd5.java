031d2a21a36f73b2e5f9dee2cda2bed6a08de53d
sm2011 <23eagle@live.com>
2013-12-02 15:07:27 +0100

    andere oplossing

---
+++ tentamen_vragen/opd5.java
@@ -0,0 +1,50 @@
+
+public class opd5 {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("de kleur is " + kleur());
+		
+		
+		
+	}
+	
+	public static String kleur()
+	{
+		
+		String  kleur = grijs;
+		int getal = 0;
+		boolean isColor;
+		
+		
+		
+		
+		if ( getal == 1){
+			return "Rood"; 
+			
+		}
+		else if (getal == 2)
+		{
+			return "Groen";
+			
+		}
+		else if (getal == 3)
+		{
+			return "blauw";
+			
+		}
+		
+		else if (isColor == true)
+		{
+			return "Donker";
+			
+		}
+		
+		
+		
+		return kleur;
+		
+		
+	}
+
+}
